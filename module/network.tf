1808f3a33084d1a755002b743da59ae2ec3d3a2f
killlllllll <104679715+killlllllll@users.noreply.github.com>
2022-08-01 10:09:04 +0530

    Update network.tf

--- module/network.tf
+++ module/network.tf
@@ -1,1 +1,22 @@
+data "aws_availability_zones" "available" {
+}
+
+resource "aws_vpc" "main" {
+  vpc_id   = ""  
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.my_vpc.id
+  subnet_id  = ""
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.my_vpc.id
+  subnet_id  = ""
+}
+
+resource "aws_internet_gateway" "my_vpc_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
 
