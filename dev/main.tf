26a3bd81f5cbe4b0043f748ae1f2e6b474eed278
killlllllll <104679715+killlllllll@users.noreply.github.com>
2022-08-02 17:47:26 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -9,23 +9,22 @@ module "react-spa-frontend-s3-cf" {
     acm_certificate_domain = "cloudtoday.click"
     zonid                  = "Z08978411J71TQQD321FO"
     env                    =  "prod123"
-    acm_certificate       = "arn:aws:acm:us-east-1:991008360267:certificate/92f2310b-6c90-4f8c-bf3f-0afc9697c3c4"
+    acm_certificate        = "arn:aws:acm:us-east-1:991008360267:certificate/92f2310b-6c90-4f8c-bf3f-0afc9697c3c4"
     bucket_name            =  "coffeeindigo-ui"
     acl                    = "private"
     versioning             = "Enabled"
 }
 
 
-
 module "strapi-cms-ecs" {
-    source           = "../../modules/services/strapi-cms-ecs/"
+    source                 = "../strapi"
     accountid              = "991008360267"
     aws_region             = "ap-south-1"
+    security_groups        =  ["sg-09a741fa74e1640d2"]
+    subnets                =  ["subnet-f7b2f1bb", "subnet-e650c59d", "subnet-1f4f5277"]
+    targetgroup_arn        =  "arn:aws:elasticloadbalancing:ap-south-1:991008360267:targetgroup/new-tg-1/5519b74f939c0ad7"
+    execution_role_arn     =  "arn:aws:iam::991008360267:role/example-ecs-task-execution"
     cluster_name           = "coffeeindigo-ecs"
-    
-    
-    
-    
     app_port               = 80
     fargate_cpu            = 1024
     fargate_memory         = 2048
