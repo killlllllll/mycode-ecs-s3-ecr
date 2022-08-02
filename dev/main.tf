a348e913e987dc9a56f03aac6204b2976692e80d
killlllllll <104679715+killlllllll@users.noreply.github.com>
2022-08-02 17:29:17 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,8 +1,31 @@
+provider "aws" {
+ region = "ap-south-1"
+}
+
+module "react-spa-frontend-s3-cf" {
+    source                 = "../cf"
+    hosted_zone            = "cloudtoday.click" 
+    domain_name            = "test.cloudtoday.click"
+    acm_certificate_domain = "cloudtoday.click"
+    zonid                  = "Z08978411J71TQQD321FO"
+    env                    =  "prod123"
+    acm_certificate       = "arn:aws:acm:us-east-1:991008360267:certificate/92f2310b-6c90-4f8c-bf3f-0afc9697c3c4"
+    bucket_name            =  "coffeeindigo-ui"
+    acl                    = "private"
+    versioning             = "Enabled"
+}
+
+
+
 module "strapi-cms-ecs" {
     source           = "../../modules/services/strapi-cms-ecs/"
     accountid              = "991008360267"
     aws_region             = "ap-south-1"
     cluster_name           = "coffeeindigo-ecs"
+    
+    
+    
+    
     app_port               = 80
     fargate_cpu            = 1024
     fargate_memory         = 2048
